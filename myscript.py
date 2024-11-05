8bad55eb2647f1145149d92f78b3a848c6d4b77e
Harry LAW-YEN <harry.lawyen@gmail.com>
2024-11-05 15:45:52 -0500

    Ajout du if dans le main.yml

--- myscript.py
+++ myscript.py
@@ -3,7 +3,8 @@ import sys
 
 badhash = sys.argv[1]
 goodhash = sys.argv[2]
-command = "git bisect start" + badhash + " " + goodhash
 
-os.system(command)
+
+os.system(f"git bisect start {badhash} {goodhash}")
 os.system("git bisect run python manage.py test")
+os.system("git bisect reset")
